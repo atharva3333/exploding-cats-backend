1c88e0e5df8c8b6746421ccfe8a107e298dd5990
Atharva <scifacts001@gmail.com>
2024-02-24 12:18:43 +0530

    working code

--- api/index.go
+++ api/index.go
@@ -1,38 +1,46 @@
+
 package handler
 
 import (
-	"encoding/json"
+	"fmt"
 	"net/http"
+
+	. "github.com/tbxark/g4vercel"
 )
 
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
-}
-
-var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
-
 func Handler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/albums" {
-		http.NotFound(w, r)
-		return
-	}
+	server := New()
 
-	switch r.Method {
-	case "GET":
-		getAlbums(w, r)
-	default:
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
-}
-
-func getAlbums(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(albums)
+	server.GET("/", func(context *Context) {
+		context.JSON(200, H{
+			"message": "hello go from vercel !!!!",
+		})
+	})
+	server.GET("/hello", func(context *Context) {
+		name := context.Query("name")
+		if name == "" {
+			context.JSON(400, H{
+				"message": "name not found",
+			})
+		} else {
+			context.JSON(200, H{
+				"data": fmt.Sprintf("Hello %s!", name),
+			})
+		}
+	})
+	server.GET("/user/:id", func(context *Context) {
+		context.JSON(400, H{
+			"data": H{
+				"id": context.Param("id"),
+			},
+		})
+	})
+	server.GET("/long/long/long/path/*test", func(context *Context) {
+		context.JSON(200, H{
+			"data": H{
+				"url": context.Path,
+			},
+		})
+	})
+	server.Handle(w, r)
 }
